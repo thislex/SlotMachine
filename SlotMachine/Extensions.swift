4e451b771b3d4d6c20537cdc26d0cb7a6ad9d911
Lexter Tapawan <lexter.tapawan@gmail.com>
2023-11-28 12:12:01 +0000

    Main content view UI implemented.

---
+++ SlotMachine/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  SlotMachine
+//
+//  Created by Lexter Tapawan on 28/11/2023.
+//
+
+import Foundation
---
+++ SlotMachine/ReelView.swift
@@ -0,0 +1,18 @@
+//
+//  ReelView.swift
+//  SlotMachine
+//
+//  Created by Lexter Tapawan on 28/11/2023.
+//
+
+import SwiftUI
+
+struct ReelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReelView()
+}
