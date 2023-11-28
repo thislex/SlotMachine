0f7862953cf585b4c37c2e0bf71b80b47df0e4d2
Lexter Tapawan <lexter.tapawan@gmail.com>
2023-11-28 10:12:08 +0000

    Initial Commit

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Lexter Tapawan on 28/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
