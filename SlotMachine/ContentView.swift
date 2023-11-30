ab83ef4d6675eabc3ce129cc5dc8d6a1876fc28c
Lexter Tapawan <lexter.tapawan@gmail.com>
2023-11-30 12:25:20 +0000

    Implemented animations.

--- SlotMachine/ContentView.swift
+++ SlotMachine/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     let symbols = ["gfx-bell", "gfx-cherry", "gfx-coin", "gfx-grape", "gfx-seven", "gfx-strawberry"]
     
-    @State private var highscore: Int = 0
+    @State private var highscore: Int = UserDefaults.standard.integer(forKey: "HighScore")
     @State private var coins: Int = 100
     @State private var betAmount: Int = 10
     @State private var reels: Array = [0, 1, 2]
@@ -20,6 +20,8 @@ struct ContentView: View {
     @State private var isActiveBet10: Bool = true
     @State private var isActiveBet20: Bool = false
     @State private var showingModal: Bool = false
+    @State private var animatingSymbol: Bool = false
+    @State private var animatingModal: Bool = false
     
     // MARK: - FUNCTIONS
     
@@ -57,6 +59,7 @@ struct ContentView: View {
     
     func newHighScore() {
         highscore = coins
+        UserDefaults.standard.set(highscore, forKey: "HighScore")
     }
     
     func playerLoses() {
@@ -82,6 +85,13 @@ struct ContentView: View {
             // SHOW MODAL WINDOW
             showingModal = true
         }
+    }
+    
+    func resetGame() {
+        UserDefaults.standard.set(0, forKey: "HighScore")
+        highscore = 0
+        coins = 100
+        activateBet10()
     }
     
     // MARK: - BODY
@@ -137,6 +147,12 @@ struct ContentView: View {
                         Image(symbols[reels[0]])
                             .resizable()
                             .modifier(ImageModifier())
+                            .opacity(animatingSymbol ? 1 : 0)
+                            .offset(y: animatingSymbol ? 0 : -50)
+                            .animation(.easeOut(duration: Double.random(in: 0.9...1.1)), value: animatingSymbol)
+                            .onAppear(perform: {
+                                self.animatingSymbol.toggle()
+                            })
                     }
                     
                     HStack (alignment: .center, spacing: 0) {
@@ -146,6 +162,12 @@ struct ContentView: View {
                             Image(symbols[reels[1]])
                                 .resizable()
                                 .modifier(ImageModifier())
+                                .opacity(animatingSymbol ? 1 : 0)
+                                .offset(y: animatingSymbol ? 0 : -50)
+                                .animation(.easeOut(duration: Double.random(in: 0.5...0.7)), value: animatingSymbol)
+                                .onAppear(perform: {
+                                    self.animatingSymbol.toggle()
+                                })
                         }
                         
                         Spacer()
@@ -156,19 +178,35 @@ struct ContentView: View {
                             Image(symbols[reels[2]])
                                 .resizable()
                                 .modifier(ImageModifier())
+                                .opacity(animatingSymbol ? 1 : 0)
+                                .offset(y: animatingSymbol ? 0 : -50)
+                                .animation(.easeOut(duration: Double.random(in: 0.7...0.9)), value: animatingSymbol)
+                                .onAppear(perform: {
+                                    self.animatingSymbol.toggle()
+                                })
                         }
                     }
                     .frame(maxWidth: 500)
                     
                     // MARK: - SPIN BUTTON
                     Button(action: {
-                        // SPIN THE REELS
+                        // 1. SET THE DEFAULT STATE: NO ANIMATION
+                        withAnimation {
+                            self.animatingSymbol = false
+                        }
+                        
+                        // 2. SPIN THE REELS WITH CHANGING THE SYMBOLS
                         self.spinReels()
                         
-                        // CHECK WINNING
+                        // 3. TRIGGER THE ANIMATION AFTER CHANGING THE SYMBOLS
+                        withAnimation {
+                            self.animatingSymbol = true
+                        }
+                        
+                        // 4. CHECK WINNING
                         self.checkWinning()
                         
-                        // GAME IS OVER
+                        // 5. GAME IS OVER
                         self.isGameOver()
                     }) {
                         Image("gfx-spin")
@@ -199,14 +237,18 @@ struct ContentView: View {
                         
                         Image("gfx-casino-chips")
                             .resizable()
+                            .offset(x: isActiveBet20 ? 0 : 20)
                             .opacity(isActiveBet20 ? 1 : 0)
                             .modifier(CasinoChipsModifier())
                     }
+                    
+                    Spacer()
                     
                     // MARK: - - BET 10
                     HStack(alignment: .center, spacing: 10) {
                         Image("gfx-casino-chips")
                             .resizable()
+                            .offset(x: isActiveBet10 ? 0 : -20)
                             .opacity(isActiveBet10 ? 1 : 0)
                             .modifier(CasinoChipsModifier())
                         
@@ -227,7 +269,7 @@ struct ContentView: View {
             .overlay(
                 // RESET
                 Button(action: {
-                    print("Reset the game")
+                    self.resetGame()
                 }) {
                     Image(systemName: "arrow.2.circlepath.circle")
                 }
@@ -281,6 +323,8 @@ struct ContentView: View {
                             
                             Button(action: {
                                 self.showingModal = false
+                                self.animatingModal = false
+                                self.activateBet10()
                                 self.coins = 100
                             }) {
                                 Text("New Game".uppercased())
@@ -297,16 +341,20 @@ struct ContentView: View {
                                     )
                             }
                         }
-                        
                         Spacer()
                     } //: VSTACK
                     .frame(minWidth: 280, idealWidth: 280, maxWidth: 320, minHeight: 260, idealHeight: 280, maxHeight: 320, alignment: .center)
                     .background(Color.white)
                     .clipShape(RoundedRectangle(cornerRadius: 20))
                     .shadow(color: Color("ColorTransparentBlack"), radius: 6, x: 0, y: 0)
+                    .opacity($animatingModal.wrappedValue ? 1 : 0)
+                    .offset(y: $animatingModal.wrappedValue ? 0 : -100)
+                    .animation(.spring(response: 0.6, dampingFraction: 1.0, blendDuration: 1.0), value: animatingModal)
+                    .onAppear(perform: {
+                        self.animatingModal = true
+                    })
                 } //: ZSTACK
             }
-            
         } //: ZSTACK
         .sheet(isPresented: $showingInfoView) {
             InfoView()
